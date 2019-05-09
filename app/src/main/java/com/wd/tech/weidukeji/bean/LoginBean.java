a7a8b63dd5f19ba9f590bfb8510dd9c81f3b903b
Administrator <1686625458@qq.com>
2019-05-09 21:29:36 +0800

    第三次测试

---
+++ app/src/main/java/com/wd/tech/weidukeji/bean/LoginBean.java
@@ -0,0 +1,9 @@
+package com.wd.tech.weidukeji.bean;
+
+/**
+ * 作者:今夕何夕
+ * 时间:${data}
+ * Description:这个是注释
+ */
+public class LoginBean {
+}
---
+++ app/src/main/java/com/wd/tech/weidukeji/presenter/MyPresenter.java
@@ -0,0 +1,9 @@
+package com.wd.tech.weidukeji.presenter;
+
+/**
+ * 作者:今夕何夕
+ * 时间:${data}
+ * Description:这个是注释
+ */
+public class MyPresenter {
+}
